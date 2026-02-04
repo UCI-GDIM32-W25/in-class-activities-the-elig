4edc4588d33acd647f286fc9b938409fa7664e97
the-elig <elidg@uci.edu>
2026-02-03 18:01:48 -0800

    activity 1

--- GDIM32 In-Class Activities/Assets/W5/Activity/W5Activity1.cs
+++ GDIM32 In-Class Activities/Assets/W5/Activity/W5Activity1.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
+
 
 public class W5Activity1 : MonoBehaviour
 {
@@ -41,18 +41,20 @@ public class W5Activity1 : MonoBehaviour
 // attacking with axe
 // axe now has 4 durability remaining
 
-public interface IBreakable {
+public interface IBreakable 
+{
 	void Damage(float damage);
 	void Break();
 }
 
-public abstract class Item {
-	public abstract void Use();
+public abstract class Item 
+{
+    protected float _durability = 5.0f;
+    public abstract void Use();
 }
 
-public class Axe : Item, IBreakable {
-	private float _durability = 5.0f;
-	
+public class Axe : Item, IBreakable 
+{	
 	public void Damage (float damage) {
 		_durability -= damage;
 		Debug.Log("axe now has " + _durability + " durability remaining");
@@ -66,17 +68,26 @@ public class Axe : Item, IBreakable {
 		Debug.Log("attacking with axe");
 		Damage(1.0f);
 	}
-}
 
-public class ElvenSword : Item {
-	public void Use () {
-		Debug.Log("attacking with Elven sword");
+	public void Break()
+	{
+		Debug.Log("axe is broken!");
 	}
 }
 
-public class Torch : Item, IBreakable {
-	
-	public void Damage (float damage) {
+public class ElvenSword : Item
+{
+	public override void Use()
+	{
+		Debug.Log("attacking with Elven sword");
+	}
+
+}
+
+public class Torch : Item, IBreakable 
+{
+	public void Damage (float damage) 
+	{
 		_durability -= damage;
 		Debug.Log("torch now has " + _durability + " durability remaining");
 		
@@ -85,14 +96,14 @@ public class Torch : Item, IBreakable {
 		}
 	}
 	
-	public void BreakItem () {
+	public void Break() 
+	{
 		Debug.Log("torch is broken!");
 	}
 	
-	protected override void Use () {
+	public override void Use() {
 		Debug.Log("lighting area with torch");
 		Damage(1.0f);
 	}
 }
 
-*/
